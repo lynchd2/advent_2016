3ffa923c98b8923c5e24dc33d6289d94615cdc90
lynchd2 <lynchd2@tcnj.edu>
2016-12-02 00:39:23 -0500

    Started second part of first challenge, complete first part of second challenge

--- example1.rb
+++ example1.rb
@@ -2,6 +2,7 @@ MAP_KEY = {}
 
 
 def shortest(input)
+	all_spots = {}
 	coordinate = [0,0]
 	direction = "north"
 	input.split(", ").each do |b|
@@ -40,12 +41,17 @@ def shortest(input)
 			else 
 				p "Failure"
 			end
-		else 
-			p "Failure"
+		end
+		if all_spots[coordinate]
+			p coordinate
+			all_spots[coordinate] += 1
+		else
+			all_spots[coordinate] = 0
 		end
 	end
 	p coordinate.inject(0) {|sum, value| sum += value.abs}
 end
 
 input = "L5, R1, R3, L4, R3, R1, L3, L2, R3, L5, L1, L2, R5, L1, R5, R1, L4, R1, R3, L4, L1, R2, R5, R3, R1, R1, L1, R1, L1, L2, L1, R2, L5, L188, L4, R1, R4, L3, R47, R1, L1, R77, R5, L2, R1, L2, R4, L5, L1, R3, R187, L4, L3, L3, R2, L3, L5, L4, L4, R1, R5, L4, L3, L3, L3, L2, L5, R1, L2, R5, L3, L4, R4, L5, R3, R4, L2, L1, L4, R1, L3, R1, R3, L2, R1, R4, R5, L3, R5, R3, L3, R4, L2, L5, L1, L1, R3, R1, L4, R3, R3, L2, R5, R4, R1, R3, L4, R3, R3, L2, L4, L5, R1, L4, L5, R4, L2, L1, L3, L3, L5, R3, L4, L3, R5, R4, R2, L4, R2, R3, L3, R4, L1, L3, R2, R1, R5, L4, L5, L5, R4, L5, L2, L4, R4, R4, R1, L3, L2, L4, R3"
+input = "R8, R4, R4, R8"
 shortest(input)
