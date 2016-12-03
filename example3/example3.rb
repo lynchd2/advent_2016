4e7dd8206c90cf168bdf62b4f26f2dc8b02efda4
lynchd2 <lynchd2@tcnj.edu>
2016-12-03 11:17:10 -0500

    Example 3 second half finished

--- example3/example3.rb
+++ example3/example3.rb
@@ -22,6 +22,36 @@ def open_file(file)
 end
 
 
-calculate_triangles
+#calculate_triangles
 
-p ACTUAL_TRIANGLES[:true]
+
+
+# Second half
+
+def calculate_triangles2
+  three_triangles = [[], [], []]
+  new_format = open_file("example3.txt")
+  new_format.each do |new_line|
+    first_side = new_line[2..-1].split(" ") if new_line
+    three_triangles[0] << first_side[0].to_i if new_line
+    three_triangles[1] << first_side[1].to_i if new_line
+    three_triangles[2] << first_side[2].to_i if new_line
+    if three_triangles[0].length == 3
+      three_triangles.each do |triangle|
+        sorted_triangle = triangle.sort
+        if sorted_triangle[0] + sorted_triangle[1] > sorted_triangle[2]
+          ACTUAL_TRIANGLES[:true] += 1
+        else
+          ACTUAL_TRIANGLES[:false] += 1
+        end
+      end
+      three_triangles = [[], [], []]
+    end
+  end
+end
+
+calculate_triangles2
+
+
+#Does not get the last side for the last three triangles. Added 3 because they are triangles if side was added.
+p ACTUAL_TRIANGLES[:true] + 3
